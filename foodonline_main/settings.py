5db943dac910dcf903df469b25b0f90e23c437be
Nazirakhan <nazirahammed98@gmail.com>
2024-06-01 01:32:12 +0530

    message alert animation feature added

--- foodonline_main/settings.py
+++ foodonline_main/settings.py
@@ -140,3 +140,14 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+from django.contrib.messages import constants as messages
+
+MESSAGE_TAGS = {
+    messages.ERROR: "danger",
+    messages.SUCCESS: "success",
+    messages.WARNING: "warning",
+    messages.INFO: "info",
+    messages.DEBUG: "dark",
+}
