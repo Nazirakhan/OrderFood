9d87a1646f063b93daf6be903b2494fd994f2c94
Nazirakhan <nazirahammed98@gmail.com>
2024-06-01 00:46:36 +0530

    customer registration success alert message feature added

--- accounts/views.py
+++ accounts/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .forms import UserForm
 from .models import User
+from django.contrib import messages
 
 # Create your views here.
 
@@ -27,7 +28,8 @@ def registerUser(request):
             user = User.objects.create_user(first_name=first_name, last_name=last_name, username=username, email=email, password=password)
             user.role = User.CUSTOMER
             user.save()
-            print('User is created')
+            messages.success(request,"User created successfully!")
+            return redirect('registerUser')
         else:
             print('Invalid form')
             print(form.errors)
