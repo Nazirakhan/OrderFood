08a86434237d56dc9cef5705b00ae7fa88b1b985
Nazirakhan <nazirahammed98@gmail.com>
2024-06-04 00:13:30 +0530

    vendor model rectified from license to licence

--- vendor/forms.py
+++ vendor/forms.py
@@ -4,4 +4,4 @@ from .models import Vendor
 class VendorForm(forms.ModelForm):
     class Meta:
         model = Vendor
-        fields = ['vendor_name', 'vendor_license']
+        fields = ['vendor_name', 'vendor_licence']
