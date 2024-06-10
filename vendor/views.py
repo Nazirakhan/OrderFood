e9d519a20b66f50d2092d975a55b91d037666bf5
Nazirakhan <nazirahammed98@gmail.com>
2024-06-10 21:32:33 +0530

    vendor profile page setup updated

--- vendor/views.py
+++ vendor/views.py
@@ -1,16 +1,29 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .forms import VendorForm
 from accounts.forms import UserProfileForm
 from .models import UserProfile
 from vendor.models import Vendor
+from django.contrib import messages
 # Create your views here.
 
 def vprofile(request):
     profile = get_object_or_404(UserProfile, user=request.user)
     vendor = get_object_or_404(Vendor, user=request.user)
 
-    profile_form = UserProfileForm(instance=profile)
-    vendor_form = VendorForm(instance=vendor)
+    if request.method == 'POST':
+        profile_form = UserProfileForm(request.POST, request.FILES, instance = profile)
+        vendor_form = VendorForm(request.POST, request.FILES, instance = vendor)
+        if profile_form.is_valid() and vendor_form.is_valid():
+            profile_form.save()
+            vendor_form.save()
+            messages.success(request, "Settings updated successfully!")
+            return redirect('vprofile')
+        else:
+            print(profile_form.errors)
+            print(vendor_form.errors)
+    else:
+        profile_form = UserProfileForm(instance=profile)
+        vendor_form = VendorForm(instance=vendor)
 
     context = {
         'profile_form': profile_form,
