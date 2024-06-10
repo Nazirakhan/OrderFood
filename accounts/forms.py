7b0ebbda844d53e43f6c4f39a6a3fb19d2b40a69
Nazirakhan <nazirahammed98@gmail.com>
2024-06-11 02:01:52 +0530

    vendor profile forced login and readonly field to latitude and longitude added

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,4 +1,8 @@
+from typing import Any, Mapping
 from django import forms
+from django.core.files.base import File
+from django.db.models.base import Model
+from django.forms.utils import ErrorList
 from .models import User, UserProfile
 from .validators import allow_only_images_validator
 
@@ -21,6 +25,20 @@ class UserForm(forms.ModelForm):
 class UserProfileForm(forms.ModelForm):
     profile_picture = forms.FileField(widget=forms.FileInput(attrs={'class': 'btn btn-info'}), validators=[allow_only_images_validator])
     cover_photo = forms.FileField(widget=forms.FileInput(attrs={'class': 'btn btn-info'}), validators=[allow_only_images_validator])
+
+    # making latitude and longitude readonly fields option-1
+    # latitude = forms.CharField(widget=forms.TextInput(attrs={'readonly':'readonly'}))
+    # longitude = forms.CharField(widget=forms.TextInput(attrs={'readonly':'readonly'}))
+
+
     class Meta:
         model = UserProfile
         fields = ['profile_picture', 'cover_photo', 'address_line_1', 'address_line_2','country','state','city', 'pincode','latitude','longitude']
+
+
+    # making latitude and longitude readonly fields option-2
+    def __init__(self, *args, **kwargs):
+        super(UserProfileForm, self).__init__(*args, **kwargs)
+        for field in self.fields:
+            if field == 'latitude' or field == 'longitude':
+                self.fields[field].widget.attrs['readonly'] = 'readonly'
