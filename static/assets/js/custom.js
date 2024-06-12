b59f5e5cf31fb7a2b5a511ce224509e662769e50
Nazirakhan <nazirahammed98@gmail.com>
2024-06-13 02:40:37 +0530

    restaurant profile and address geocoding done

--- static/assets/js/custom.js
+++ static/assets/js/custom.js
@@ -6,7 +6,7 @@ autocomplete = new google.maps.places.Autocomplete(
     {
         types: ['geocode', 'establishment'],
         //default in this app is "IN" - add your country code
-        componentRestrictions: {'country': ['in']},
+        componentRestrictions: {'country': ['in', 'us']},
     })
 // function to specify what should happen when the prediction is clicked
 autocomplete.addListener('place_changed', onPlaceChanged);
@@ -28,7 +28,7 @@ function onPlaceChanged (){
     var address = document.getElementById('id_address').value;
 
     geocoder.geocode({'address': address}, function(results, status){
-        console.log('results=>', results);
+        // console.log('results=>', results);
         // console.log('status=>', status)
         if(status == google.maps.GeocoderStatus.OK){
             var latitude = results[0].geometry.location.lat();
@@ -43,4 +43,28 @@ function onPlaceChanged (){
     });
 
     // loop through the address components and assign other address data
+    console.log(place.address_components);
+    if (place.address_components){
+        for(var i=0; i<place.address_components.length; i++){
+            for(var j=0; j<place.address_components[i].types.length; j++){
+                // get country
+                if(place.address_components[i].types[j] == 'country'){
+                    $('#id_country').val(place.address_components[i].long_name);
+                };
+                //  get state
+                if(place.address_components[i].types[j] == 'administrative_area_level_1'){
+                    $('#id_state').val(place.address_components[i].long_name);
+                };
+                if(place.address_components[i].types[j] == 'locality'){
+                    $('#id_city').val(place.address_components[i].long_name);
+                };
+                if(place.address_components[i].types[j] == 'postal_code'){
+                    $('#id_pincode').val(place.address_components[i].long_name);
+                }else{
+                    $('#id_pincode').val("");
+                };
+            }
+        }
+    }
+
 }
